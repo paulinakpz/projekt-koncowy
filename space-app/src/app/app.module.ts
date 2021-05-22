9efda8b9192336739a791a0bfb6b47484541ae17
paulina-kpz <paulina.kwapisz6@gmail.com>
2021-05-22 19:22:18 +0200

    dodanie sidenav

--- space-app/src/app/app.module.ts
+++ space-app/src/app/app.module.ts
@@ -7,12 +7,17 @@ import { SpaceModule } from './space/space.module';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import {MatButtonModule} from '@angular/material/button';
-
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule} from '@angular/material/icon';
+import { MatDividerModule} from '@angular/material/divider';
+import { AboutComponent } from './about/about.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
@@ -20,8 +25,11 @@ import {MatButtonModule} from '@angular/material/button';
     SpaceModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    MatButtonModule
-    
+    MatButtonModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatDividerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
